e753a104f2b4bb68723878bafb7ac3104fdc289b
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2025-07-07 18:34:47 +0100

    first

---
+++ R/tornado.R
@@ -0,0 +1,204 @@
+#' @importFrom rlang .data
+NULL
+
+#' Tornado sampling and plotting function
+#'
+# -------------------------------------------------------------------------
+#' A small wrapper around lapply that loops over a functions parameters,
+#' sampling one whilst fixing the others. `tornado_sample()` just wraps a
+#' simple univariate sensitivity analysis, ensuring the output is formatted in a
+#' usable way for the namesake plot function.
+#'
+# -------------------------------------------------------------------------
+#' @returns
+#'
+#' For `tornado_sample()` a tibble with class **tornado_samples** and an
+#' additional attribute that gives the baseline value calculated when setting
+#' each argument to it's distributions mean value (or an optional,
+#' user-supplied, value). For `tornado_plot()`, a `ggplot2` object
+#'
+# -------------------------------------------------------------------------
+#' @name tornado
+NULL
+
+# -------------------------------------------------------------------------
+#' @param n Number of samples.
+#'
+#' @param fun A vectorised function.
+#'
+#' @param distributions Distributions created with the
+#' [distributions3](https://alexpghayes.github.io/distributions3/) package that
+#' correspond to the named parameters of `fun`.
+#'
+#' @param baseline NULL or a scalar numeric value. If not NULL then this value
+#' is used as the comparitor when producing the latter tornado plots.
+#'
+#' @param output_name The name to use of the result column in the output
+#' tibbles. This must not correspond to any of the parameter names of `fun`.
+#'
+#' @param ... Not currently used.
+#'
+# -------------------------------------------------------------------------
+#' @examples
+#'
+#' # User inputs ----------------------------------------------------------
+#'
+#' # Function with desired arguments
+#' # NOTE: MUST be vectorised by the user and return a scalar value
+#' fun <- function(cost_per_day, days, discount) cost_per_day * days * discount
+#'
+#' # A distribution (from distributions3) for each function argument
+#' distributions <- list(
+#'     cost_per_day = distributions3::Gamma(shape = 9, rate = 0.5),
+#'     days         = distributions3::Gamma(shape = 5, rate = 1),
+#'     discount     = distributions3::Beta(alpha = 2, beta = 40)
+#' )
+#'
+#' # Number of samples to take for each argument
+#' samples <- 1000
+#'
+#' # Package functionality ------------------------------------------------
+#'
+#' # generate the samples for each parameter
+#' dat <- tornado_sample(samples, fun, distributions)
+#'
+#' # jitter plot
+#' tornado_plot(dat)
+#'
+#' # plot of the extremes (akin to the more traditional plots)
+#' tornado_plot(dat, type = "maxmin")
+#'
+# -------------------------------------------------------------------------
+#' @rdname tornado
+#' @export
+tornado_sample <- function(n, fun, distributions, ..., baseline = NULL, output_name = ".result") {
+
+    rlang::check_dots_empty0(...)
+    stopifnot(
+        is.function(fun),
+        is.numeric(n) && length(n) == 1L && !is.na(n) && n %% 1 == 0,
+        is.list(distributions),
+        all(vapply(distributions, distributions3::is_distribution, TRUE)),
+        is.character(output_name) && length(output_name) == 1L && !is.na(output_name)
+    )
+    funargs <- methods::formalArgs(fun)
+    funargs <- funargs[funargs != "..."]
+    distnms <- names(distributions)
+    stopifnot(setequal(funargs, distnms) && length(distnms) == length(funargs))
+    if (output_name %in% distnms) {
+        stop("`output_name` (%s) must be different to the parameter names of `fun`. ", output_name)
+    }
+
+    # calculate the distribution means
+    means <- lapply(distributions, mean)
+
+    # calculate the baseline output
+    if (is.null(baseline)) {
+        baseline <- do.call(fun, means)
+    }
+    stopifnot(is.numeric(baseline) && length(baseline) == 1L && !is.na(baseline))
+
+    # calculate the sample outputs
+    sample_outputs <- lapply(seq_along(distributions), function(i) {
+        inputs <- means
+        varying <- list(.varying = distnms[[i]])
+        X <- distributions[[i]]
+        random_inputs <- distributions3::random(X, n)
+        inputs[[i]] <- random_inputs
+        results <- do.call(fun, inputs)
+        if (length(results) != length(random_inputs)) {
+            stop("Incompatible outputs. Your function must be vectorised returning length n outputs for length n inputs.")
+        }
+        out <- c(inputs, list(do.call(fun, inputs)))
+        names(out)[length(out)] <- output_name
+        tibble::as_tibble(out)
+    })
+    names(sample_outputs) <- distnms
+
+    structure(
+        sample_outputs,
+        baseline = baseline,
+        class = "tornado_samples"
+    )
+}
+
+# -------------------------------------------------------------------------
+#' @param x Output from `tornado_sample()`.
+#'
+#' @param type Character string. Either 'jitter' or 'maxmin'. If 'jitter' then
+#' we simply plot the sample results against the corresponding distribution that
+#' varies. Otherwise, we plot the corresponding maximum and minimum values as a
+#' split bar chart.
+#'
+#' @param nbreaks Passed to `scales::breaks_extended()`.
+#'
+#' @param xlab The label for the x-axis.
+#'
+# -------------------------------------------------------------------------
+#' @rdname tornado
+#'
+# -------------------------------------------------------------------------
+#' @export
+tornado_plot <- function(x, ..., type = c("jitter", "maxmin"), nbreaks = 6, xlab = "value") {
+
+    rlang::check_dots_empty0(...)
+    stopifnot(
+        inherits(x, "tornado_samples"),
+        is.numeric(nbreaks) && length(nbreaks) == 1L && !is.na(nbreaks) && nbreaks %% 1 == 0,
+        is.character(xlab) && length(xlab) == 1L && !is.na(xlab)
+    )
+    type <- match.arg(type)
+
+    dat <- do.call(rbind, x)
+    baseline <- attr(x, "baseline")
+    negative <- dat$.result < baseline
+    names <- rep(names(x), times = vapply(x, nrow, 1L))
+
+    # calculate rough plot limits
+    lower <- max(dat[negative, ".result", drop = TRUE])
+    lower <- -ceiling(lower * 1.2)
+    upper <- max(dat[!negative, ".result", drop = TRUE])
+    upper <- ceiling(upper * 1.2)
+
+    dat <- data.frame(value = dat$.result, varying = names, negative = negative)
+
+    if (type == "jitter") {
+        breaks <- scales::breaks_extended(n = nbreaks)(c(lower, upper))
+        ggplot2::ggplot(dat, ggplot2::aes(.data$value, .data$varying, col = .data$negative)) +
+            ggplot2::geom_jitter() +
+            ggplot2::theme_minimal() +
+            ggplot2::theme(
+                axis.title.y = ggplot2::element_blank(),
+                legend.position = "none"
+            ) +
+            ggplot2::xlab(xlab) +
+            ggplot2::geom_vline(xintercept = baseline, linetype = "dashed") +
+            ggplot2::coord_cartesian(xlim = c(lower, upper)) +
+            ggplot2::scale_x_continuous(breaks = breaks, labels = breaks)
+    } else {
+
+        dat$plot_value <- dat$value - baseline
+        dat <- stats::aggregate(
+            plot_value ~ varying + negative,
+            data = dat,
+            FUN = function(x) if (x[1L] < 0) min(x) else (max(x))
+        )
+        breaks <- scales::breaks_extended(n = nbreaks)(c(lower, upper)) - baseline
+        labels <- breaks + baseline
+        ggplot2::ggplot(dat, ggplot2::aes(.data$plot_value, .data$varying, fill = .data$negative)) +
+            ggplot2::geom_col() +
+            ggplot2::theme_minimal() +
+            ggplot2::theme(
+                axis.title.y = ggplot2::element_blank(),
+                legend.position = "none"
+            ) +
+            ggplot2::xlab(xlab) +
+            ggplot2::geom_vline(xintercept = 0, linetype = "dashed") +
+            ggplot2::coord_cartesian(xlim = c(lower, upper) - baseline) +
+            ggplot2::scale_x_continuous(
+                breaks = breaks,
+                labels = labels
+            )
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,141 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# tp
+
+**Experimental, demo package**
+
+Small wrapper package, primarily to demonstrate creation of tornado
+plots.
+
+## Installation
+
+You can install the development version of tp from R-Universe via:
+
+``` r
+
+install.packages('tp', repos = c('https://timtaylor.r-universe.dev', 'https://cloud.r-project.org'))
+#> Installing package into '/tmp/RtmpudCKzO/temp_libpath49df68a48e89'
+#> (as 'lib' is unspecified)
+#> Warning: package 'tp' is not available for this version of R
+#> 
+#> A version of this package for your version of R might be available elsewhere,
+#> see the ideas at
+#> https://cran.r-project.org/doc/manuals/r-patched/R-admin.html#Installing-packages
+```
+
+## How to use
+
+tp provides two main functions: `tornado_samples()` and
+`tornado_plot()`.
+
+The first of these, `tornado_samples()` takes:
+
+- A user-supplied, vectorised, input function;
+- A set of distributions you wish to draw from that have a one-to-one
+  matching to the aforementioned function’s parameters. These must be
+  from the
+  [distributions3](https://alexpghayes.github.io/distributions3/)
+  package.
+- A number of samples to draw for each parameter.
+
+The function is just a small wrapper around lapply that loops over a
+functions parameters, sampling one whilst fixing the others. In effect
+it just wraps a simple univariate sensitivity analysis, ensuring the
+output is formatted in a usable way for the later plot function. The
+output is a tibble with class **tornado_samples** and an additional
+attributes that gives the baseline value calculated when setting each
+argument to it’s distributions mean value.
+
+``` r
+library(tp)
+set.seed(1)
+
+# Function with desired arguments
+# NOTE: MUST be vectorised by the user and return a scalar value
+fun <- function(cost_per_day, days, discount) cost_per_day * days * discount
+
+# A distribution (from distributions3) for each function argument
+distributions <- list(
+    cost_per_day = distributions3::Gamma(shape = 9, rate = 0.5),
+    days         = distributions3::Gamma(shape = 5, rate = 1),
+    discount     = distributions3::Beta(alpha = 2, beta = 40)
+)
+
+# Number of samples to take for each argument
+samples <- 1000
+
+# generate the samples for each parameter
+(dat <- tornado_sample(samples, fun, distributions))
+#> $cost_per_day
+#> # A tibble: 1,000 × 4
+#>    cost_per_day  days discount .result
+#>           <dbl> <dbl>    <dbl>   <dbl>
+#>  1        13.5      5   0.0476    3.22
+#>  2        25.6      5   0.0476    6.10
+#>  3        25.2      5   0.0476    6.01
+#>  4        19.5      5   0.0476    4.64
+#>  5         9.21     5   0.0476    2.19
+#>  6        20.0      5   0.0476    4.75
+#>  7        21.6      5   0.0476    5.14
+#>  8        20.5      5   0.0476    4.89
+#>  9        15.3      5   0.0476    3.63
+#> 10        12.7      5   0.0476    3.01
+#> # ℹ 990 more rows
+#> 
+#> $days
+#> # A tibble: 1,000 × 4
+#>    cost_per_day  days discount .result
+#>           <dbl> <dbl>    <dbl>   <dbl>
+#>  1           18  7.73   0.0476    6.62
+#>  2           18  2.69   0.0476    2.31
+#>  3           18  4.65   0.0476    3.99
+#>  4           18  2.56   0.0476    2.20
+#>  5           18  2.76   0.0476    2.36
+#>  6           18  2.19   0.0476    1.87
+#>  7           18  5.84   0.0476    5.01
+#>  8           18  3.45   0.0476    2.96
+#>  9           18  8.31   0.0476    7.12
+#> 10           18  4.47   0.0476    3.83
+#> # ℹ 990 more rows
+#> 
+#> $discount
+#> # A tibble: 1,000 × 4
+#>    cost_per_day  days discount .result
+#>           <dbl> <dbl>    <dbl>   <dbl>
+#>  1           18     5   0.0493   4.43 
+#>  2           18     5   0.0541   4.87 
+#>  3           18     5   0.0434   3.90 
+#>  4           18     5   0.0177   1.60 
+#>  5           18     5   0.0109   0.977
+#>  6           18     5   0.0551   4.96 
+#>  7           18     5   0.0179   1.61 
+#>  8           18     5   0.0711   6.40 
+#>  9           18     5   0.0840   7.56 
+#> 10           18     5   0.0531   4.77 
+#> # ℹ 990 more rows
+#> 
+#> attr(,"baseline")
+#> [1] 4.285714
+#> attr(,"class")
+#> [1] "tornado_samples"
+```
+
+`tornado_plot()` takes, as input, the output from `tornado_sample()` as
+well as a character string specifying the type of tornado plot to create
+(**jitter** or **maxmin**). The first of these simply plots the sample
+results against the corresponding distribution that varies. The second
+options plots the corresponding maximum and minimum values as a split
+bar chart.
+
+``` r
+tornado_plot(dat)
+```
+
+<img src="man/figures/README-unnamed-chunk-4-1.png" width="100%" />
+
+``` r
+tornado_plot(dat, type = "maxmin")
+```
+
+<img src="man/figures/README-unnamed-chunk-4-2.png" width="100%" />
